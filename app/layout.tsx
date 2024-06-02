b9becbd89d5c140a87b778be10ea020f467bbac9
Luís Gomes <luis252001@hotmail.com>
2024-06-02 17:32:30 -0300

    feat: add nav component to page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "@styles/globals.css";
+import { Nav } from "@components";
 
 export const metadata = {
   title: "Promptopia",
@@ -12,7 +13,10 @@ const RootLayout = ({ children }: React.PropsWithChildren) => {
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   );
